9446cf009aa9328a7dcaa042c230d25014baad11
Peter stromdahl <110845677+Stpeterd@users.noreply.github.com>
2025-08-24 00:03:39 -0500

    creating layout

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
+# Rental time sheet
+# customer name
+# tool
+# time 4hr min pick 4 hrs from start time ex. 1pm - 5pm 
+# price 
+# taxes
+# total
 
+
+
+
+import tkinter as tk
+
+window = tk.Tk()
+
+window.title("Rental Time Sheet")
+window.geometry("450x450")
+
+
+
+window.mainloop()

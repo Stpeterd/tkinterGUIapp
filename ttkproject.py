aa827c0a7b7d564ac56e42cbd9d32bd50df7f776
Peter stromdahl <110845677+Stpeterd@users.noreply.github.com>
2025-08-31 23:09:54 -0500

    new preject

---
+++ ttkproject.py
@@ -0,0 +1,19 @@
+# pip install ttkbootstrap
+
+import ttkbootstrap as ttk 
+from ttkbootstrap.constants import *
+
+root = ttk.Window(themename="darkly")
+
+def open_dialog():
+    app = ttk.Topleve(title="My Toplevel")
+    app.mainloop()
+
+open_button = ttk.Button(root, text="0pen", command=open_dialog, bootstyle=SUCCES)
+open_button.pack(side=LEFT, padx=5, pady=10)
+
+exit_button = ttk.Button(root, text="Exit", bootstyle=(INFO, OUTLINE))
+
+exit_button.pack(side=LEFT, padx=5, pady=10)
+
+root.mainloop()
